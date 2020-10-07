15bdc7711d5cf3480f078ccace10eaf9e2550350
leolucianox <leonardo.silva@zappts.com>
2020-10-07 00:21:30 -0300

    css/ inicio do mobile

--- client/src/components/Body/FilterBar/SelectedInput/style.js
+++ client/src/components/Body/FilterBar/SelectedInput/style.js
@@ -1,27 +1,29 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-    color:#fff;
-    min-width:4rem;
-    height:70%;
-    display:flex;
-    flex-direction:row;
-    align-items:center;
-    justify-content:space-between;
-    background-color:#C4C4C4;
-    margin:0 1rem 0 1rem;
+  background: #1890ff !important;
+  border-radius: 20px;
+  padding: 0 10px;
+  color: #fff;
+  min-width: 4rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #c4c4c4;
+  margin: 0 1rem 0 1rem;
 
-    label{
-        margin: 0 10px;
-    }
+  label {
+    margin: 0 10px;
+  }
 
-    div{
-        margin:0 0 0 6px;
-        width:10px;
-        height:100%;
-        display:flex;
-        flex-direction:column;
-        align-items:center;
-        justify-content:flex-start;
-    }
+  div {
+    margin: 0 0 0 6px;
+    width: 10px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+  }
 `;
--- client/src/components/Body/FilterBar/style.js
+++ client/src/components/Body/FilterBar/style.js
@@ -1,84 +1,90 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-    width:100%;
-    height:12%;
-    background-color:#fff;
-    display:flex;
-    flex-direction:column;
-    justify-content:center;
+  width: 100%;
+  height: 12%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 
-    hr {
-        border-top: 3px solid #bbb;
-    }
+  hr {
+    border-top: 3px solid #bbb;
+  }
 `;
 export const BottomDiv = styled.div`
-    width:100%;
-    height:50%;
-    background-color:#fff;
-    display:flex;
-    flex-direction:row;
-    justify-content:flex-start;
-    align-items:center;
+  background: #1976d252;
+  width: -webkit-fill-available;
+  padding: 10px 10px;
+  border-radius: 10px;
+  margin-top: 5px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
 
-    input{
-        cursor:pointer;
-        margin:0 1rem;
-    }
+  input {
+    cursor: pointer;
+    margin: 0 10px 0 0;
+    border-radius: 50px;
+    padding: 0 10px;
+  }
 
-    label{
-        cursor:pointer;
-    }
+  label {
+    cursor: pointer;
+  }
 
-    div{
-        cursor:pointer;
-        margin:0 1rem;
-    }
+  div {
+    cursor: pointer;
+    margin: 0 1.5rem;
+  }
 
-    datalist{
-        cursor:pointer;
-        margin:0 1rem;
-    }
+  datalist {
+    cursor: pointer;
+    margin: 0 1rem;
+  }
 `;
 
 export const Divider = styled.div`
-    width:100%;
-    height:2px;
-    background-color:#C4C4C4;
-    margin:0.2rem 0 0.2rem;
+  width: 100%;
+  height: 2px;
+  background-color: grey;
+  padding-bottom: 2px;
 `;
 
 export const UpperDiv = styled.div`
-    height:45%;
-    display:flex;
-    flex-direction: row;
-    align-items:center;
+  padding-bottom: 1%;
+  height: 45%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 `;
 
 export const UpperDivLeft = styled.div`
-    height:100%;
-    width:50%;
-    display:flex;
-    flex-direction: row;
-    align-items:center;
+  height: 100%;
+  width: 50%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 `;
 
-export const UpperDivRight = styled.div`
-    height:100%;
-    width:50%;
-    display:flex;
-    flex-direction: row;
-    align-items:center;
-    justify-content:flex-end;
+export const FilterButton = styled.div`
+  height: 100%;
+  width: 50%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
 
-    button{
-        margin:0 0.5rem 0 0 ;
-        background-color:#1890ff;
-        color:#fff;
-        /* border: 0.5px solid black; */
-    }
+  button {
+    border-radius: 50px;
+    padding: 5px 20px;
+    border: none;
+    background-color: #1976d2;
+    color: #fff;
+    /* border: 0.5px solid black; */
+  }
 
-    button:focus{
-        outline:none;
-    }
+  button:focus {
+    outline: none;
+  }
 `;
